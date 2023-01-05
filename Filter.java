f2e0f852944f89f75908c754b44f5b1ea7cdc0c1
Raushan <rk733564@gmail.com>
2023-01-05 16:44:56 +0530

    First Commit

---
+++ Filter.java
@@ -0,0 +1,27 @@
+// Find the even number using stream.
+
+package streamConcept;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Filter {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> l1 = new ArrayList<Integer>();
+		l1.add(72);
+		l1.add(23);
+		l1.add(84);
+		l1.add(22);
+		l1.add(12);
+		l1.add(55);
+		l1.add(12);
+		l1.add(66);
+		System.out.println(l1);
+		List<Integer> l2 = l1.stream().filter(i -> i % 2 == 0).toList();
+		System.out.println("Even numbers " + l2);
+
+	}
+
+}
